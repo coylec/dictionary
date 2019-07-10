c852c5f8519b1b824f17c47b450112a18b1c132b
Conrad Coyle <coylec@users.noreply.github.com>
2019-07-10 13:12:34 +0100

    updates for non existing words and case sensitivity

--- app1.py
+++ app1.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open("data.json"))
 
 def describe(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "The word does not exist."
 
 word = input("Enter word: ")
 
