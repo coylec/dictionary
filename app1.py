a1e047a43169b755f66bcb5d013ca287b21947e5
Conrad Coyle <coylec@users.noreply.github.com>
2019-07-10 13:51:13 +0100

    return best matches and user confirmation

--- app1.py
+++ app1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,14 @@ def describe(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        confirm = input("Did you mean %s? Enter Y for yes or N for no: " % get_close_matches(word, data.keys())[0])
+        if confirm == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif confirm == "N":
+            return "The word does not exist, please double check it."
+        else:
+            return "We do not understand your entry."
     else:
         return "The word does not exist."
 
